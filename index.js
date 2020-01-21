dcbcb7e8596d0c841dbe6c20421d68fb32a726cf
Skaffen-Amtiskaw <skaffen@Skaffen-Amtiskaws-MacBook-Pro.local>
2020-01-20 22:04:36 -0500

    debug

--- index.js
+++ index.js
@@ -32,4 +32,4 @@ app.use((req, res, next) => {
     res.status(404).send('ノಠ益ಠノ彡┻━┻  404 page')
 });
 
-app.listen(os.Getenv("PORT"), () => console.log('Server Up and running'));
+app.listen(process.env.PORT, () => console.log('Server Up and running'));
